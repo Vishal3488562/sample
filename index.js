9015971c929a52f7ef27c25f090aa9343c5eff08
Vishal Gupta <vishal3488562@gmail.com>
2024-09-15 12:38:01 +0530

    add hello

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const btn = document.querySelector('button');
+
+btn.addEventListener("click", () => {
+    const heading = document.querySelector('h2');
+
+    heading.innerHTML = "WelCome To You"
+})
+
